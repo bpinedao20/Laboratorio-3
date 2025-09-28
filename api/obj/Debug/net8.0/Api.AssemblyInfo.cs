1bf678b66905f6e6b8475556d7f33e8543867ffa
bpinedao20 <bpinedao@miumg.edu.gt>
2025-09-27 21:09:52 -0600

    Clone del proyecto de Baloncesto pero para la tarea laboratorio 3

--- api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a0a3c52bdc5de445e5e7796f04f939449c52d7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0602a0318c5f7371fae8a1b34acf03e07a647c84")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
